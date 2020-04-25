23de576503d855729db8ed2e0c38292edf53e31e
Okula-Yenugu <okula.yenugu@gmail.com>
2020-04-24 20:03:14 -0500

    My project setup completed successfully

---
+++ shoppingbackend/src/main/java/net/oyenugu/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.oyenugu.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
